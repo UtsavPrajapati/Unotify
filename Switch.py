193bc225e0b2f20e1ea905135e90ff4def870bbc
Utsav prajapati <prajapatiutxav@gmail.com>
2017-05-07 07:14:10 +0545

    Update Switch.py

--- Switch.py
+++ Switch.py
@@ -5,13 +5,17 @@ class SwitchWindow:
     def __init__(self):
         self.address = read_from_database()[0]
         self.tk = tk
-        self.stat = True
         window = tk.Tk()
         window.protocol("WM_DELETE_WINDOW", self.do_nothing)
         self.window = window
         window.title("Unotify")
         tk.Label(window, text="      The app is currently:     ").pack()
-        self.text = tk.Label(window, text="ON", fg="green")
+        file_data = self.read_from_file()
+        if file_data == "True":
+            self.stat = True
+        if file_data == "False":
+            self.stat = False
+        self.text = tk.Label(window, text=file_data, fg="green")
         self.text.pack()
         tk.Button(window, text="SWITCH", command=self.switch).pack()
         tk.Label(window, text="Logged as " + self.address).pack()
@@ -42,6 +46,12 @@ class SwitchWindow:
         f = open("switch", "w")
         f.write(text)
         f.close()
+
+    def read_from_file(self):
+        f = open("switch", "w")
+        a = f.read()
+        f.close()
+        return a
 
 if "downloads" in os.listdir(os.getcwd()):
     SwitchWindow()
